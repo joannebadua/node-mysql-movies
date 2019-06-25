6e1ebbaf5fc9b946baa7d4d755a208e9ada4904e
Joanne Badua <joannebadua@gmail.com>
2019-06-25 02:40:53 -0700

    pseudocode instructions, require mysql+inq, create connection w mysql

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+//first display acll of the items available for sale. 
+//Include the ids, names, and prices of products for sale.
+
+
+//prompt users with two messages.
+//The first should ask them the ID of the product they would like to buy.
+//The second message should ask how many units of the product they would like to buy.
+
+
+// check if your store has enough of the product to meet the customer's request.
+
+//If not, the app should log a phrase like Order Unable to be Fulfilled! Stock Manager Contacted, 
+//and then prevent the order from going through.
+
+//if your store does have enough of the product, 
+//you should fulfill the customer's order.
+
+//updating the SQL database to reflect the remaining quantity.
+//Once the update goes through, 
+//show the customer the total cost of their purchase.
