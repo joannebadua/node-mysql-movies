e39ae181958515c462b15982446a2558252539eb
Joanne Badua <joannebadua@gmail.com>
2019-06-25 02:58:21 -0700

    git ignore file, npm init and install mysql and inquirer,function queryAllProducts to print out items available for sale

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,47 @@
-//first display acll of the items available for sale. 
-//Include the ids, names, and prices of products for sale.
+//require mysql + inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+//create connection w mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon"
+  });
+  connection.connect(function(err) {
+      if (err) throw err;
+      console.log("connected as id " + connection.threadId);
+      queryAllProducts();
+  });
+
+  //first display acll of the items available for sale. 
+//Include the ids, names, and prices of products for sale
+//to be printed within dtb to terminal
+
+  function queryAllProducts() {
+      //hand off a query string - string literal argument - 
+      connection.query("SELECT * FROM products", 
+      //callback function to be excuted once query has been executed
+      function(err, res) {
+          if (err) throw err;
+          //reiterate through the response and format
+          for (var i = 0; i < res.length; i++) {
+              //alt: separate function that takes in an array and formats
+              console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res.[i].stock_quantity + " | " + res[i].department_name + " | ")
+          }  
+              console.log("----------------------------------");
+          });
+        
+
+  
+
 
 
 //prompt users with two messages.
