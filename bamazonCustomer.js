3a7133b3e174142694b0d73adb9805fc90653ebf
Joanne Badua <joannebadua@gmail.com>
2019-06-27 18:06:35 -0700

    add prompt for quantity and logic to update goods based on customer's request

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,75 +4,118 @@ var inquirer = require("inquirer");
 var Table = require("cli-table2");
 //create connection w mysql
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "password",
-    database: "bamazon"
-  });
-  connection.connect(function(err) {
-      if (err) throw err;
-      console.log("connected as id " + connection.threadId);
-      viewAllProducts();
-  });
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  viewAllProducts();
+});
 
 //first display all of the items available for sale. 
 //Include the ids, names, and prices of products for sale
 //to be printed within dtb to terminal
 function viewAllProducts() {
-var q = "SELECT * FROM goods";
-console.log("--------------------------------------------------------------------");
-console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
-console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
-console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
-console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
-console.log("--------------------------------------------------------------------");
-var table = new Table({
-  head: ["id", "Product_name", "Price", "Stock_quantity", "Department_name"],
-  colWidths: [5, 70, 10, 20],
-  style:{
-    head: ["gold"]
-  }
-});
+  var q = "SELECT * FROM goods";
+  console.log("----------------------------------------------------------------------------------------------------------");
+  console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
+  console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
+  console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
+  console.log("This is a tribute to the performers on the beautiful stages from Broadway to the screen. ");
+  console.log("----------------------------------------------------------------------------------------------------------");
+  var table = new Table({
+    head: ["id", "Product_name", "Price", "Stock_quantity", "Department_name"],
+    colWidths: [5, 70, 10, 20],
+    style: {
+      head: ["gold"]
+    }
+  });
 
-connection.query(q, function (err, res) {
+  connection.query(q, function (err, res) {
     if (err) throw err;
     // console.log(res);
-//reiterate through the response and format
-for (var i = 0; i < res.length; i++) {
-//alt: separate function that takes in an array and formats
-table.push([res[i].id, res[i].Product_name, res[i].Price, res[i].Stock_quantity, res[i].Department_name]);
-};
-    console.log(table.toString());  
+    //reiterate through the response and format
+    for (var i = 0; i < res.length; i++) {
+      //alt: separate function that takes in an array and formats
+      table.push([res[i].id, res[i].Product_name, res[i].Price, res[i].Stock_quantity, res[i].Department_name]);
+    };
+    console.log(table.toString());
     console.log("--------------------------------------------------------------------");
-  customerSelect();
+    customerSelect();
   });
-  
+
 };
 
 function customerSelect() {
   inquirer
-      .prompt(
-          {
+    .prompt(
+      [{
         name: "productId",
         type: "input",
         message: "Please select the ID of the prized item you would like to buy."
       },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many you would like to buy."
-      }) 
-      .then(function(inquirerResponse) {
-        var query = connection.query(
-          "SELECT * FROM goods WHERE id ?", inquirerResponse.productId, function(err, res){
-            console.log(res);
-        });
+        // {
+        //   name: "quantity",
+        //   type: "input",
+        //   message: "How many you would like to buy."
+        // }
+      ]
+    )
+    //dont forget to close the [] arrays bc the prompt knows there are two questions. 
+    .then(function (inquirerResponse) {
+      // console.log(inquirerResponse);
+      //below is the pass inquirer response.pronct id is the answer, 
+      //and give it to the new function in 81 and show up as a placeholder. this will become idSelected
+      quantitySelect(inquirerResponse.productId);
+    });
+}
+function quantitySelect(idSelected) {
+  // console.log("inside our quantity function", idSelected);
+  //database call to ask how many to buy
+  connection.query(
+        "SELECT * FROM goods WHERE id = " + idSelected, function(err, res){
+          // console.log(res[0].Product_name, err);
+          howManyPrompt(res[0]);
+          //res is the goods. the index 0 to pick this whole object from the array then .product_name
       });
+}
+function howManyPrompt(productToBuy) {
+// console.log("productToBuy", productToBuy);
+inquirer.prompt (
+  [
+    {
+      name: "quantity",
+      type: "input",
+      message: productToBuy.Product_name + "... How many you would like to buy."
     }
+  ]
+)
+.then(function (inquirerResponse) {
+  // console.log(inquirerResponse); 
+  if (inquirerResponse.quantitySelect > productToBuy.Stock_quantity) {
+console.log("Not enough in stock")
+  }
+  else {
+    console.log("You got it! Enjoy!")
+    //from ln 97 quanityt
+    var newTotal = parseInt(productToBuy.Stock_quantity) - parseInt(inquirerResponse.quantity);
+    // console.log("newTotal", newTotal)
+    // console.log("productToBuy", productToBuy)
+    // console.log("inquirerResponse", inquirerResponse)
+    var query = "UPDATE goods SET Stock_quantity = " + newTotal + " WHERE id = " + productToBuy.id;
+    connection.query(query, function(err, res){
+// console.log(err, res)
+    });
+  }
+});
+}
